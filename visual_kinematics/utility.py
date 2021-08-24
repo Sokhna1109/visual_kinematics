4ec141a32f5d874189ace0eb2f5265ba80d8f511
Sokhna Samb <sokhna.samb@etu.sorbonne-universite.fr>
2021-08-24 13:30:38 +0200

    utility opt

--- visual_kinematics/utility.py
+++ visual_kinematics/utility.py
@@ -1,8 +1,10 @@
 import numpy as np
 from math import pi
+from numba import jit
 
 
 # ================== constrain angle between -pi and pi
+@jit(nopython=True)
 def simplify_angle(angle):
     while angle > pi:
         angle -= 2 * pi
@@ -12,6 +14,7 @@ def simplify_angle(angle):
 
 
 # ================== constrain angles[n, ] between -pi and pi
+@jit(nopython=True)
 def simplify_angles(angles):
     for i in range(angles.shape[0]):
         angles[i] = simplify_angle(angles[i])
